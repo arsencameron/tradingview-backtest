7026c00c0d5e50d8e8b311c9e93db1cf94c5f15a
Arsen Cameron <arsencameron@Arsens-MacBook-Pro.local>
2025-02-24 00:07:45 -0500

    test chart

---
+++ src/components/ChartComponent.jsx
@@ -0,0 +1,44 @@
+import { AreaSeries, createChart, ColorType } from 'lightweight-charts';
+import React, { useEffect, useRef } from 'react';
+
+const ChartComponent = ({ data, colors = {} }) => {
+    const {
+        backgroundColor = 'white',
+        lineColor = '#2962FF',
+        textColor = 'black',
+        areaTopColor = '#2962FF',
+        areaBottomColor = 'rgba(41, 98, 255, 0.28)',
+    } = colors;
+
+    const chartContainerRef = useRef();
+
+    useEffect(() => {
+        const chart = createChart(chartContainerRef.current, {
+            layout: {
+                background: { type: ColorType.Solid, color: backgroundColor },
+                textColor,
+            },
+            width: chartContainerRef.current.clientWidth,
+            height: 300,
+        });
+
+        chart.timeScale().fitContent();
+        const newSeries = chart.addSeries(AreaSeries, { lineColor, topColor: areaTopColor, bottomColor: areaBottomColor });
+        newSeries.setData(data);
+
+        const handleResize = () => {
+            chart.applyOptions({ width: chartContainerRef.current.clientWidth });
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+            chart.remove();
+        };
+    }, [data, backgroundColor, lineColor, textColor, areaTopColor, areaBottomColor]);
+
+    return <div ref={chartContainerRef} />;
+};
+
+export default ChartComponent;
